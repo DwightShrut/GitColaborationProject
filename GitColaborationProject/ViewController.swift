b604341833d7064382a118a9a83bd3af5d4697e2
DwightShrut <skatemax@mail.ru>
2025-06-14 18:36:39 +0300

    Clean Project

--- GitColaborationProject/ViewController.swift
+++ GitColaborationProject/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  GitColaborationProject
-//
-//  Created by Макс Макеев on 12.06.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
